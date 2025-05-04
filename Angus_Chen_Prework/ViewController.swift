5fc88df6973a702e06892d74da5eb053b5b21011
Angus <anguschen03@gmail.com>
2025-05-04 03:12:42 -0400

    Added prework files to repo

---
+++ Angus_Chen_Prework/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Angus_Chen_Prework
+//
+//  Created by Angus Chen on 5/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // main function call
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        }
+    
+    // helper function
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
+}
+
